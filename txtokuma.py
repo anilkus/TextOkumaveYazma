036c62bafd6e7f7565cef428a21dbe0a59ca069f
Anıl Kuş <anilkus01@gmail.com>
2022-08-17 12:50:13 +0300

    Update txtokuma.py

--- txtokuma.py
+++ txtokuma.py
@@ -5,7 +5,7 @@ Created on Tue Aug 16 17:46:40 2022
 @author: AnilKus
 """
 
-with open ('C:/Users/LENOVO/Desktop/text.txt',encoding='utf8') as f:
+with open ('Dosya Yolu',encoding='utf8') as f:
     for line in f:
         print(line.strip())
     
